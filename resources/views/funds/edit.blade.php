d5c2eaa1d0858ebc90ce234df585cc6f5b498f8e
Joe Koop <joe@joekoop.com>
2025-01-06 07:54:59 -0600

    Fixed #21

--- resources/views/funds/edit.blade.php
+++ resources/views/funds/edit.blade.php
@@ -8,4 +8,25 @@
 		<input name="name" type="text" value="{{ old('name', $fund?->name) }}" required maxlength="255" />
 		<button type="submit">Save</button>
 	</form>
+
+	<h2>Transactions</h2>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Date</th>
+				<th>Amount</th>
+				<th>Description</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($fund->transactions()->orderByDesc('date')->get() as $transaction)
+				<tr>
+					<td>{{ $transaction->date->format('Y-m-d') }}</td>
+					<td>{{ $transaction->amount }}</td>
+					<td>{{ $transaction->getDescription() }}</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
 @endsection
