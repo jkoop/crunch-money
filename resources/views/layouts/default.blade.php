359dc926739276e00013ee1feec2efe09eee95a3
Joe Koop <joe@joekoop.com>
2025-01-06 18:07:42 -0600

    Update footer link from "Contribute" to "To Do List" in default layout
    This change improves navigation by directing users to the issues page for better tracking of tasks and contributions.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -49,7 +49,7 @@
 	@if (Auth::check())
 		- <a href="{{ route('logout') }}">Logout</a>
 	@endif
-	- <a href="https://github.com/jkoop/crunch-money" target="_blank">Contribute</a>
+	- <a href="https://github.com/jkoop/crunch-money/issues" target="_blank">To Do List</a>
 	@if (config('app.service_status.url'))
 		- <a href="{{ config('app.service_status.url') }}" target="_blank">Service Status</a>
 	@endif
