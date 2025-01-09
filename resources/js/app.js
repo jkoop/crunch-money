2606f61bdfbf72962fef66f4510a686221792d89
Joe Koop <joe@joekoop.com>
2025-01-08 19:40:04 -0600

    Refactor date input handling in budget edit view and update Prettier configuration
    - Modified the date input field in the budget edit view to set its value dynamically using a new `getToday` function, enhancing user experience by automatically populating the current date.
    - Updated the Prettier configuration to enforce consistent code formatting, including setting `semi` to true for better readability.
    
    This commit improves the usability of the budget edit form and ensures code consistency across the project.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,13 @@
-import "./bootstrap"
-import Alpine from "alpinejs"
+import "./bootstrap";
+import Alpine from "alpinejs";
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
+Alpine.start();
 
-Alpine.start()
+window.getToday = () => {
+	const date = new Date();
+	date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+	return date.toISOString().split("T")[0];
+};
+
+window.dispatchEvent(new Event("js-ready"));
--- resources/views/budgets/edit.blade.php
+++ resources/views/budgets/edit.blade.php
@@ -17,7 +17,12 @@
 		@csrf
 		<input name="negate" type="hidden" value="1" />
 		<input name="budget_id" type="hidden" value="{{ $budget->id }}" />
-		<input name="date" type="date" value="{{ now()->format('Y-m-d') }}" required />
+		<input name="date" type="date" required />
+		<script>
+			window.addEventListener("js-ready", () => {
+				document.querySelector('input[name="date"]').value = getToday();
+			});
+		</script>
 		<input name="amount" type="number" style="width: 100px;" step="0.01" required autofocus
 			placeholder="withdraw amount" />
 		<input name="description" type="text" required placeholder="description" maxlength="255" />
